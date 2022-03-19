ecef52332b1cdb655dd1cbd567cd99e854a715b3
MohammadJ <javadi.Mohammad128@gmail.com>
2022-03-19 22:02:08 +0330

    add demo.gif

--- dev/serve.vue
+++ dev/serve.vue
@@ -12,13 +12,10 @@ export default defineComponent({
 </script>
 
 <template>
-  <div id="app" style="width: 100%; box-sizing: border-box;">
+  <div id="app" style="width: 500px; box-sizing: border-box;">
     <Tab>
-      <TabItem title="Tab1" >
-        This is Tab1
-      </TabItem> 
-      <TabItem title="Tab2" active>
-        <p v-for="i in 30">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum earum dolorum voluptate deserunt perspiciatis modi incidunt magnam, sed explicabo amet consectetur, quisquam tempore libero enim maiores esse non necessitatibus aliquam.</p>
+      <TabItem v-for="n in 9" :title="`Tab${n}`" >
+        <p v-for="i in 3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum earum dolorum voluptate deserunt perspiciatis modi incidunt magnam, sed explicabo amet consectetur, quisquam tempore libero enim maiores esse non necessitatibus aliquam.</p>
       </TabItem> 
     </Tab>
   </div>
